69d3868e32faca73f1fc8d85ef2faba8a981c3c7
Trextercon <daryl.chong@hotmail.com>
2026-02-13 18:23:17 +0800

    added demo bg, n bk to top button

--- scroll.js
+++ scroll.js
@@ -26,9 +26,27 @@ if (demoVideo) {
                 }
             });
         },
-        { threshold: 0.6 } // plays when 60% visible
+        { threshold: 0.6 }
     );
 
     videoObserver.observe(demoVideo);
 }
+
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 400) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
 
